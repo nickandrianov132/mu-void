3a95cb48947978c6e31d62ea77667869337b83f5
Nick Andrianov <nickandrianov132@gmail.com>
2025-08-16 18:02:10 +0000

    upd api.js and public.js url

--- client/src/services/public.js
+++ client/src/services/public.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = 'http://localhost:7000/api';
+const baseUrl = 'http://46.202.188.234:7000/api';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: baseUrl,
