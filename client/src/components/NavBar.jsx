350dfdec9e57b1e94641b3383e6743ef43d11e5d
Nick Andrianov <nickandrianov132@gmail.com>
2025-08-30 15:58:40 +0000

    NavBar component replace to NavLink and CharCard css fix

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,77 +1,46 @@
-import { useNavigate } from 'react-router';
+import { NavLink } from 'react-router';
 import { ABOUT_ROUTE, DOWNLOAD_ROUTE, GUIDE_ROUTE, HOME_ROUTE, RANKINGS_ROUTE, REGISTRATION_ROUTE } from '../utils/constants';
 import {GiHouse, GiSecretBook, GiSwordsEmblem, GiCharacter, GiSpellBook, GiCloudDownload} from 'react-icons/gi';
 
 const NavBar = () => {
-    const navigate = useNavigate()
 
     return (
         <div className='nav_container'>
             <div className='nav_list'>
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(HOME_ROUTE)}
-                >
+                <NavLink className="nav_list_item_wraper" to={HOME_ROUTE}>
                     <GiHouse className='giHouse'/>
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(HOME_ROUTE)}
-                    >Home</a>
-                </div>
+                    <span>Home</span>
+                </NavLink>
                 <span className='nav_list_span'></span>
 
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(ABOUT_ROUTE)}
-                >
+                <NavLink className="nav_list_item_wraper" to={ABOUT_ROUTE}>
                     <GiSpellBook className='giSpellBook'/>
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(ABOUT_ROUTE)}
-                    >About</a>
-                </div>
+                    <span>About</span>
+                </NavLink>
                 <span className='nav_list_span'></span>
 
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(DOWNLOAD_ROUTE)}
-                >
+                <NavLink className="nav_list_item_wraper" to={DOWNLOAD_ROUTE}>
                     <GiCloudDownload className='giDownload' />
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(DOWNLOAD_ROUTE)}
-                    >Download</a>
-                </div>
+                    <span>Download</span>
+                </NavLink>
                 <span className='nav_list_span'></span>
 
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(REGISTRATION_ROUTE)}
-                >
+                <NavLink className="nav_list_item_wraper" to={REGISTRATION_ROUTE}>
                     <GiCharacter className='giCharacter'/>
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(REGISTRATION_ROUTE)}
-                    >Registration</a>
-                </div>
+                    <span>Registration</span>
+                </NavLink>
                 <span className='nav_list_span'></span>
 
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(GUIDE_ROUTE)}
-                >
-                    <GiSecretBook className='giSecretBook'/>  
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(GUIDE_ROUTE)}
-                    >Guide</a>
-                </div>
+                <NavLink className="nav_list_item_wraper" to={GUIDE_ROUTE}>
+                    <GiSecretBook className='giSecretBook'/>
+                    <span>Guide</span>
+                </NavLink>
                 <span className='nav_list_span'></span>
 
-                <div className='nav_list_item_wraper'
-                    onClick={() => navigate(RANKINGS_ROUTE)}
-                >
-                    <GiSwordsEmblem className='giSwordEmblem'/>
-                    <a 
-                        className='nav_list_item' 
-                        // onClick={() => navigate(RANKINGS_ROUTE)}
-                    >Rankings</a>
-                </div>
+                <NavLink className="nav_list_item_wraper" to={RANKINGS_ROUTE}>
+                    <GiSwordsEmblem className='giSwordEmblem' />
+                    <span>Rankings</span>
+                </NavLink>
             </div>
         </div>
     );
--- client/src/components/modals/userPanel/SectionLinks.jsx
+++ client/src/components/modals/userPanel/SectionLinks.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { ACCOUNT_VIP_ROUTE } from "../../../utils/constants";
 
 const SectionLinks = () => {
@@ -6,7 +6,7 @@ const SectionLinks = () => {
     return (
         <div className="user_panel_links">
                 <a className="link-vote" href="#">Vote & get WCoins</a>
-                <a className="link-vip" onClick={() => navigate(ACCOUNT_VIP_ROUTE)}>Buy VIP</a>
+                <NavLink className="link-vip" to={ACCOUNT_VIP_ROUTE} >Buy VIP</NavLink>
                 <a className="link-discord" href="https://discord.com/invite/9gXnu6n8" target="_blank">Discord</a>
                 {/* <a className="link-report" href="#">Report Bug</a>  */}
         </div>
