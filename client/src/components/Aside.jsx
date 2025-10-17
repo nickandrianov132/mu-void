673939b72c309deb51a1b81773eafb5dbf1e1489
Nick Andrianov <nickandrianov132@gmail.com>
2025-10-17 00:27:20 +0000

    deleted player

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -3,7 +3,6 @@ import DiscordWidget from './modals/discordWidget/DiscordWidget';
 import Online from './modals/Online';
 import BannersBoard from './modals/topBanners/BannersBoard';
 import TopPlayers from './modals/TopPlayers';
-import Player from './Player';
 
 const Aside = () => {
     return (
@@ -13,7 +12,6 @@ const Aside = () => {
             <CastleInfo />
             <DiscordWidget />
             <BannersBoard />
-            <Player />
         </aside>
     );
 }
