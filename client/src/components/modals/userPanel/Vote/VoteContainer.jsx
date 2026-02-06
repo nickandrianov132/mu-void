579acdf567972acf2a48095cbba1063fb2cf6922
Nick Andrianov <nickandrianov132@gmail.com>
2026-02-06 14:32:27 +0100

    wcoins remark edited

--- client/src/components/modals/userPanel/Vote/VoteContainer.jsx
+++ client/src/components/modals/userPanel/Vote/VoteContainer.jsx
@@ -21,7 +21,7 @@ const VoteContainer = () => {
         <div className="vote_container">
             <div className="vote_header_wrapper">
                 {/* <h4 className="vote_h4">👍📢 Vote for our Server and get reward <em className="vote_em"> WCoins</em>!</h4> */}
-                <h4 className="vote_h4"> <em className="vote_em"> Wcoins rewards will be available after server launching</em></h4>
+                <h4 className="vote_h4"> <em className="vote_em"> Wcoins rewards will be available from te day of server launching</em></h4>
             </div>
             {isSuccess &&
             <>
